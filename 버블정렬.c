2e23c3469e28c68fcaa7b7b6e2983520f88dd33a
username <81845513+june3011@users.noreply.github.com>
2021-10-23 21:24:20 +0900

    STACK, BubbleSort
    2021-10-23

---
+++ 버블정렬.c
@@ -0,0 +1,29 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+////void swap(int* xp, int* yp) { // 스왑 함수
+////	int temp = *xp;
+////	*xp = *yp;
+////	*yp = temp;
+////}
+//
+//void bubble1(int a[], int n) {
+//	for (int i = 0; i < n - 1; i++) {
+//		for (int j = 1; j < n - i; j++) {
+//			if (a[j - 1] > a[j]) { //버블정렬
+//				//if( a[i] > a[j] ) //선택정렬
+//				int t = a[j - 1];
+//				a[j - 1] = a[j];
+//				a[j] = t;
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int a[5] = { 5,2,3,4,1 };
+//	bubble1(a, 5);
+//	printf("%d %d %d %d %d", a[0], a[1], a[2], a[3], a[4]);
+//
+//}
